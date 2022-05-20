03a7f18c0d857b535d8bcc929a73b1d2e0296252
Аббас Гусенов <gusenov@live.ru>
2022-05-20 12:52:24 +0600

    Несколько логгеров совместно использующих один файл (sink).

---
+++ multiple-loggers-sharing-same-sink/main.cc
@@ -0,0 +1,27 @@
+// https://github.com/gabime/spdlog/wiki/1.-QuickStart#create-a-multiple-loggers-all-sharing-the-same-file-sink-aka-categories
+
+#include <iostream>
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/daily_file_sink.h"
+
+int main(int, char* [])
+{
+    try
+    {
+        auto daily_sink = std::make_shared<spdlog::sinks::daily_file_sink_mt>("logfile", 23, 59);
+        // create synchronous  loggers
+        auto net_logger = std::make_shared<spdlog::logger>("net", daily_sink);
+        auto hw_logger  = std::make_shared<spdlog::logger>("hw",  daily_sink);
+        auto db_logger  = std::make_shared<spdlog::logger>("db",  daily_sink);      
+
+        net_logger->set_level(spdlog::level::critical); // independent levels
+        hw_logger->set_level(spdlog::level::debug);
+         
+        // globally register the loggers so the can be accessed using spdlog::get(logger_name)
+        spdlog::register_logger(net_logger);
+    }
+    catch (const spdlog::spdlog_ex& ex)
+    {
+        std::cout << "Log initialization failed: " << ex.what() << std::endl;
+    }
+}
