0a52ecc56d236bc1cbd7637a15d1a29d7ffa43f3
Аббас Гусенов <gusenov@live.ru>
2022-05-20 13:00:12 +0600

    Create stdout/stderr logger object.

---
+++ create-stdout-stderr-logger-object/README.md
@@ -0,0 +1,3 @@
+```
+[2019-09-30 16:17:39.807] [console] [info] loggers can be retrieved from a global registry using the spdlog::get(logger_name)
+```
---
+++ create-stdout-stderr-logger-object/main.cc
@@ -0,0 +1,14 @@
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+void stdout_example()
+{
+    // create color multi threaded logger
+    auto console = spdlog::stdout_color_mt("console");
+    auto err_logger = spdlog::stderr_color_mt("stderr");
+    spdlog::get("console")->info("loggers can be retrieved from a global registry using the spdlog::get(logger_name)");
+}
+
+int main(int argc, const char * argv[]) {
+    stdout_example();
+    return 0;
+}
